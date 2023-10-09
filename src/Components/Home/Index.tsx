68c75c2b59ab316803cbfca918cadd410eee584c
Emmacodec <danielemmanuel7553@gmail.com>
2023-10-09 17:01:02 +0100

    using motion.div create a new component which accepts a function tha takes us to our desired page

--- src/Components/Home/Index.tsx
+++ src/Components/Home/Index.tsx
@@ -21,7 +21,9 @@ const Home = ({setSelectedPage}: Props) => {
   return (
     <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
       {/*image and header*/}
-      <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+      <motion.div 
+      onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
         {/*header*/}
         <div className='z-10 mt-32 md:basis-3/5'>
           {/*heading*/}
@@ -43,7 +45,13 @@ const Home = ({setSelectedPage}: Props) => {
           </motion.div>
 
           {/*buttons*/}
-          <div className='mt-8 flex items-center gap-8'>
+          <motion.div 
+          initial="hidden" 
+          whileInView='visible' 
+          viewport={{once:true, amount:0.5}} 
+          transition={{delay: 0.2, duration: 2}}
+          variants={{hidden:{opacity: 0, x: -50}, visible:{opacity: 1, x: 0},}}
+          className='mt-8 flex items-center gap-8'>
             <Buttons setSelectedPage={setSelectedPage}>
               Enroll Now.!!
             </Buttons>
@@ -54,14 +62,14 @@ const Home = ({setSelectedPage}: Props) => {
             >
               Discover More
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/*image*/}
         <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
           <img src={HomePageGraphic} alt='home-page-image' />
         </div>
-      </div>
+      </motion.div>
 
       {/*sponsors*/}
       {useMediaQuey && (
